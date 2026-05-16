fa64b4fda827fe094ab40f710105bc748e24775c
maxxmoto <s.evsin@my18.ru>
2026-05-16 23:58:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,6 @@ from fpdf.enums import XPos, YPos
 
 
 # ---------- АВТОСКАЧИВАНИЕ ШРИФТА ----------
-FONTS_DIR = "fonts"
 FONT_PATH = 'DejaVuSans.ttf'
 if not os.path.exists(FONT_PATH):
     os.makedirs(FONTS_DIR, exist_ok=True)
